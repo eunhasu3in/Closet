dc5967935d2fb3b232a7e599cadf4d03e563aa46
shineunjeong <dkfah1229@naver.com>
2021-05-16 00:48:01 +0900

    [추가 구현한 내용] 바텀 네비게이션 구현 홈, 패셔니스타(전문가), 내 옷장, 피드(일반인), 마이페이지 총 5가지 메뉴 각 메뉴 아이템 클릭 시 각각의 프레그먼트로 교체 ---------------------------------------------------------------- [다음 구현예정] 1) 내 옷장에 UI 구현 후 카메라, 갤러리 구현해놓은거 합치기 2) 사진 편집 기능 구현

--- app/src/main/java/com/example/closet/MainActivity.kt
+++ app/src/main/java/com/example/closet/MainActivity.kt
@@ -10,8 +10,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
+import android.util.Log
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.core.content.FileProvider
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.gun0912.tedpermission.PermissionListener
 import com.gun0912.tedpermission.TedPermission
 import kotlinx.android.synthetic.main.activity_main.*
@@ -22,32 +25,71 @@ import java.lang.Exception
 import java.text.SimpleDateFormat
 import java.util.*
 
-
-
 class MainActivity : AppCompatActivity() {
 
+    // 메인액티비티 클래스가 가지고 있는 멤버들
+    private lateinit var homeFragment: HomeFragment
+    private lateinit var fashionistaFragment: FashionistaFragment
+    private lateinit var closetFragment: ClosetFragment
+    private lateinit var feedFragment: FeedFragment
+    private lateinit var mypageFragment: MypageFragment
+
+    companion object {
+        const val TAG : String = "로그"
+   }
+
+    // 화면이 메모리에 올라갔을 때
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // 레이아웃과 연결
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "MainActivity - onCreate() called")
+        bottom_nav.setOnNavigationItemSelectedListener(onBottomNavItemSeletedListener)
         // setPermission() // 권한을 체크하는 메소드 수행
-
-
-
-
+        homeFragment = HomeFragment.newInstance()
+        supportFragmentManager.beginTransaction().add(R.id.fragments_frame, homeFragment).commit() // add는 프레그먼트 추가해주는 것
     }
 
 
+    // 바텀 네비게이션 아이템 클릭 리스너 설정
+    private val onBottomNavItemSeletedListener = BottomNavigationView.OnNavigationItemSelectedListener {
+        // when은 코틀린에서 switch문
+        when(it.itemId) {
+            R.id.menu_home -> {
+                Log.d(TAG, "MainActivity - 홈버튼 클릭!")
+                homeFragment = HomeFragment.newInstance()
+                supportFragmentManager.beginTransaction().replace(R.id.fragments_frame, homeFragment).commit() // replace는 다른 프레그먼트로 교체해주는 것
+            }
+            R.id.menu_fashionista -> {
+                Log.d(TAG, "MainActivity - 패셔니스타 버튼 클릭!")
+                fashionistaFragment = FashionistaFragment.newInstance()
+                supportFragmentManager.beginTransaction().replace(R.id.fragments_frame, fashionistaFragment).commit() // replace는 다른 프레그먼트로 교체해주는 것
+            }
+            R.id.menu_closet -> {
+                Log.d(TAG, "MainActivity - 옷장 버튼 클릭!")
+                closetFragment = ClosetFragment.newInstance()
+                supportFragmentManager.beginTransaction().replace(R.id.fragments_frame, closetFragment).commit() // replace는 다른 프레그먼트로 교체해주는 것
+            }
+            R.id.menu_feed -> {
+                Log.d(TAG, "MainActivity - 피드 버튼 클릭!")
+                feedFragment = FeedFragment.newInstance()
+                supportFragmentManager.beginTransaction().replace(R.id.fragments_frame, feedFragment).commit() // replace는 다른 프레그먼트로 교체해주는 것
+            }
+            R.id.menu_mypage -> {
+                Log.d(TAG, "MainActivity - 마이페이지 버튼 클릭!")
+                mypageFragment = MypageFragment.newInstance()
+                supportFragmentManager.beginTransaction().replace(R.id.fragments_frame, mypageFragment).commit() // replace는 다른 프레그먼트로 교체해주는 것
+            }
+        } // when문 끝
 
+        true
 
+    }
 
-
-
-
-
-
-
-
-
-
-
+//    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+//        Log.d(HomeFragment.TAG, "MainActivity - onNavigationItemSelected() called")
+//        // when은 코틀린에서 switch문
+//
+//    }
 }
